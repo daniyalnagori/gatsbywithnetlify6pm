c6afbd298680d1c2a3509a43d6c5ad982f8348c8
daniyalnagori <daniyalnagori@yahoo.com>
2020-09-06 19:25:05 +0500

    added environment variables

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,10 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+ require('dotenv').config({
+   path: `.env.${process.env.NODE_ENV}`
+ })
+
 module.exports = {
   /* Your site config here */
   plugins: [
@@ -12,7 +16,7 @@ module.exports = {
       options: {
         spaceId: `wgxsuk9hdesc`,
         // Learn about environment variables: https://gatsby.dev/env-vars
-        accessToken: "fyB8tdIXo5q6XGSvfVgbbetvwBGX9JLp1wx2-_obIvw",
+        accessToken: process.env.ACCESS_TOKEN,
       },
     },
   ],
